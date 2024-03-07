9651befd607867f0cc5f7a49819de993ca27f7ab
Nicolas Tavares dos Santos <77157262+nic-san@users.noreply.github.com>
2024-03-07 02:45:34 +0000

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 function Home(){
-    var casadodu = "<span>Primeira foto juntos 😍:<img src='contents/imgs/casadodu.jpg'/></span>";
-    var suspiro = "<span>Foto fofa demaisss 😍:<img src='contents/imgs/suspiro.jpg'/></span>";
-    var passeimal = "<span>Meu nervosismo atoa 🤣:<img src='contents/imgs/passeimal.jpg'/></span>";
-    var violao = "<span>Você canta muitooooo 😊:<img src='contents/imgs/violao.jpg'/></span>";
-    var alianca = "<span>Nossa primeira vitória como casal 🙌💕💑:<img src='contents/imgs/alianca.jpg'/></span>";
-    var texto = "<h1>💕💕💕💕💕Ouuuu, mor, eu te amo muuuuuuitão! Se você me ama muuuuuuitão me manda uma mensagem falando! 💕💕💕💕💕</h1>";
-    var html = texto+casadodu+suspiro+passeimal+violao+alianca;
+    var casadodu = <span>Primeira foto juntos 😍: <img src='contents/imgs/casadodu.jpg'/></span>;
+    var suspiro = <span>Foto fofa demaisss 😍: <img src='contents/imgs/suspiro.jpg'/></span>;
+    var passeimal = <span>Meu nervosismo atoa 🤣: <img src='contents/imgs/passeimal.jpg'/></span>;
+    var violao = <span>Você canta muitooooo 😊: <img src='contents/imgs/violao.jpg'/></span>;
+    var alianca = <span>Nossa primeira vitória como casal 🙌💕💑:<img src='contents/imgs/alianca.jpg'/></span>;
+    var texto = <h1>💕💕💕💕💕Ouuuu, mor, eu te amo muuuuuuitão! Se você me ama muuuuuuitão me manda uma mensagem falando! 💕💕💕💕💕</h1>;
+    var html = texto+""+casadodu+""+suspiro+""+passeimal+""+violao+""+alianca;
     return html;
 }
 
