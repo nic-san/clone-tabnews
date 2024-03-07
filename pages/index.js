54d827dc8234fe9adafd2c96a5ada48fe754d975
Nicolas Tavares dos Santos <77157262+nic-san@users.noreply.github.com>
2024-03-07 02:39:07 +0000

    modifica a página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home(){
-    return <h1>💕💕💕💕💕Ouuuu, mor, eu te amo muuuuuuitão! Se você me ama muuuuuuitão me manda uma mensagem falando! 💕💕💕💕💕</h1>
+    var casadodu = <span>Primeira foto juntos 😍:<img src='contents/imgs/casadodu.jpg'/></span>;
+    var suspiro = <span>Foto fofa demaisss 😍:<img src='contents/imgs/suspiro.jpg'/></span>;
+    var passeimal = <span>Meu nervosismo atoa 🤣:<img src='contents/imgs/passeimal.jpg'/></span>;
+    var violao = <span>Você canta muitooooo 😊:<img src='contents/imgs/violao.jpg'/></span>;
+    var alianca = <span>Nossa primeira vitória como casal 🙌💕💑:<img src='contents/imgs/alianca.jpg'/></span>;
+    var texto = <h1>💕💕💕💕💕Ouuuu, mor, eu te amo muuuuuuitão! Se você me ama muuuuuuitão me manda uma mensagem falando! 💕💕💕💕💕</h1>;
+    return texto+casadodu+suspiro+passeimal+violao+alianca;
 }
 
 export default Home;
