e8bd42ad6c7e556fa7fbea3beb4ea644000858c8
Nicolas Tavares dos Santos <77157262+nic-san@users.noreply.github.com>
2024-03-07 02:25:05 +0000

    modifica novamente a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Ouuuu, mor, eu te amo muuuuuuuuuuuuuuuitãoo! Se você me ama muuuuuuuuuuuuuuuuitão me manda uma mensagem falando!</h1>
+    return <h1>💕💕💕💕💕Ouuuu, mor, eu te amo muuuuuuitão! Se você me ama muuuuuuitão me manda uma mensagem falando! 💕💕💕💕💕</h1>
 }
 
 export default Home;
