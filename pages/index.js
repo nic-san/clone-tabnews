dbb06af285700acbc479e7be5be51651c37eba8a
Nicolas Tavares dos Santos <77157262+nic-san@users.noreply.github.com>
2024-03-07 02:22:54 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Correção</h1>
+    return <h1>Ouuuu, mor, eu te amo muuuuuuuuuuuuuuuitãoo! Se você me ama muuuuuuuuuuuuuuuuitão me manda uma mensagem falando!</h1>
 }
 
 export default Home;
